dccbe2980c7425f35f378140337267e872961e39
Lucas Rebellato <lrebellato15@gmail.com>
2025-11-21 23:52:15 -0300

    Add global query filter for soft deletes
    Introduced a global query filter in TodoContext to automatically exclude entities implementing ISoftDeletable where IsDeleted is true. This ensures soft-deleted records are not returned in queries.

--- TodoApi.DataAccess/Context/TodoContext.cs
+++ TodoApi.DataAccess/Context/TodoContext.cs
@@ -1,6 +1,8 @@
 using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
 using TodoApi.DataAccess.Context.EntityConfigurations;
 using TodoApi.Domain.Domain;
+using TodoApi.Domain.Interfaces;
 
 public class TodoContext : DbContext
 {
@@ -12,6 +14,31 @@ public class TodoContext : DbContext
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        base.OnModelCreating(modelBuilder);
         modelBuilder.ApplyConfiguration(new TodoListConfiguration());
+
+        foreach (var entityType in modelBuilder.Model.GetEntityTypes())
+        {
+            if (typeof(ISoftDeletable).IsAssignableFrom(entityType.ClrType))
+            {
+                var parameter = Expression.Parameter(entityType.ClrType, "e");
+                var propertyMethodInfo = typeof(EF).GetMethod("Property")!
+                    .MakeGenericMethod(typeof(bool));
+
+                var isDeletedProperty = Expression.Call(
+                    propertyMethodInfo,
+                    parameter,
+                    Expression.Constant("IsDeleted"));
+
+                var compareExpression = Expression.Equal(
+                    isDeletedProperty,
+                    Expression.Constant(false));
+
+                var lambda = Expression.Lambda(compareExpression, parameter);
+
+                modelBuilder.Entity(entityType.ClrType)
+                    .HasQueryFilter(lambda);
+            }
+        }
     }   
 }
